7226252c4826687526a7f9f82a7dfcf742835e22
Taylor Le Fustec <taylorelliott96@icloud.com>
2018-10-12 22:46:45 +0100

    removed Route::auth from web file. Not needed as make:auth not run.

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,3 @@ Route::resource('posts', 'PostsController');
 Route::resource('tasks', 'TasksController');
 Route::resource('comments', 'CommentsController');
 Route::post('posts/{post}/comments', 'CommentsController@store');
-Auth::routes();
